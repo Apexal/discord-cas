e7db12ae630639e440cbffda0073633512de8df8
Frank Matranga <thefrankmatranga@gmail.com>
2020-09-16 17:08:35 -0400

    Basic Flask CAS application

---
+++ README.md
@@ -0,0 +1,6 @@
+# Discord + CAS
+
+### Environment Variables
+- `DISCORD_SERVER_ID`
+- `DISCORD_BOT_TOKEN`
+- `FLASK_SECRET_KEY`
---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, g, session, request, render_template, redirect, url_for
+from flask_cas import CAS, login_required, logout
+from dotenv import load_dotenv
+
+# Load .env into os.environ
+load_dotenv()
+
+app = Flask(__name__)
+cas = CAS(app, '/cas')
+
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+app.config['CAS_SERVER'] = 'https://cas-auth.rpi.edu/cas'
+app.config['CAS_AFTER_LOGIN'] = '/'
+
+
+@app.route('/')
+@login_required
+def index():
+    return 'Hello, ' + cas.username
