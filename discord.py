8cab41962c4fec819b42a5c52dcd278d794f5f50
Frank Matranga <thefrankmatranga@gmail.com>
2020-09-16 17:39:15 -0400

    Complete flow

--- discord.py
+++ discord.py
@@ -8,13 +8,20 @@ BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 CLIENT_ID = os.environ.get('DISCORD_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('DISCORD_CLIENT_SECRET')
 REDIRECT_URI = os.environ.get('DISCORD_REDIRECT_URI')
+SERVER_ID = os.environ.get('DISCORD_SERVER_ID')
+VERIFIED_ROLE_ID = os.environ.get('DISCORD_VERIFIED_ROLE_ID')
 HEADERS = {
-    'Authorization': 'Bot ' + DISCORD_BOT_TOKEN,
+    'Authorization': 'Bot ' + BOT_TOKEN,
 }
+
+OAUTH_URL = f'https://discord.com/api/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&response_type=code&scope=guilds.join%20identify'
 
 
 def get_tokens(code):
-    '''Given an authorization code, request the access and refresh tokens for a Discord user. Returns the tokens. Throws an error if invalid request.'''
+    '''
+    Given an authorization code, request the access and refresh tokens for a Discord user.
+    Returns the tokens. Throws an error if invalid request.
+    '''
     response = requests.post(f'{API_BASE}/oauth2/token',
                              data={
                                  'client_id': CLIENT_ID,
@@ -31,3 +38,55 @@ def get_tokens(code):
     response.raise_for_status()
     tokens = response.json()
     return tokens
+
+
+def get_user_info(access_token):
+    '''
+    Given an access token, get a Discord user's info including id, username, discriminator, avatar url, etc.
+    Throws an error if invalid request.
+    '''
+    response = requests.get(f'{API_BASE}/users/@me', headers={
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'
+    }
+    )
+    response.raise_for_status()
+    user = response.json()
+    return user
+
+
+def add_user_to_server(access_token: str, user_id: str, nickname: str):
+    '''
+    Given a Discord user's id, add them to the Discord server with their nickname
+    set as their RCS ID and with the verified role.
+    '''
+    response = requests.put(f'{API_BASE}/guilds/{SERVER_ID}/members/{user_id}',
+                            json={
+                                'access_token': access_token,
+                                'nick': nickname,
+                                'roles': [VERIFIED_ROLE_ID],
+                            },
+                            headers=HEADERS
+                            )
+    response.raise_for_status()
+    return response
+
+
+def set_member_nickname(user_id: str, nickname: str):
+    '''Given a Discord user's id, set their nickname on the server.'''
+    response = requests.patch(f'{API_BASE}/guilds/{SERVER_ID}/members/{user_id}',
+                              json={
+                                  'nick': nickname
+                              },
+                              headers=HEADERS
+                              )
+    response.raise_for_status()
+    return response
+
+
+def add_role_to_member(user_id: str, role_id: str):
+    '''Add a role (identified by its id) to a member.'''
+    response = requests.put(
+        f'{API_BASE}/guilds/{SERVER_ID}/members/{user_id}/roles/{role_id}', headers=HEADERS)
+    response.raise_for_status()
+    return response
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, g, session, request, render_template, redirect, url_for
 from flask_cas import CAS, login_required, logout
 from dotenv import load_dotenv
 
-from discord import DISCORD_BOT_TOKEN
+from discord import OAUTH_URL, VERIFIED_ROLE_ID, get_tokens, get_user_info, add_user_to_server, add_role_to_member, set_member_nickname
 
 # Load .env into os.environ
 load_dotenv()
@@ -19,4 +19,31 @@ app.config['CAS_AFTER_LOGIN'] = '/'
 @app.route('/')
 @login_required
 def index():
-    return 'Hello, ' + cas.username
+    return f'<a href="{OAUTH_URL}">Connect Discord</a>'
+
+
+@app.route('/discord/callback', methods=['GET'])
+@login_required
+def discord_callback():
+    # Extract code or error from URL
+    authorization_code = request.args.get('code')
+    error = request.args.get('error')
+
+    # Exchange authorization code for tokens
+    tokens = get_tokens(authorization_code)
+
+    # Get info on the Discord user that just connected (really only need id)
+    discord_user = get_user_info(tokens['access_token'])
+
+    nickname = 'Placeholder Nickname'
+
+    # Add them to the server
+    add_user_to_server(tokens['access_token'], discord_user['id'], nickname)
+
+    # Set their nickname
+    set_member_nickname(discord_user['id'], nickname)
+
+    # Give them the verified role
+    add_role_to_member(discord_user['id'], VERIFIED_ROLE_ID)
+
+    return tokens['access_token']
