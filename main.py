3f8b1fc1fe0d22d5bc036b829ff4fccb6c7c6a55
Frank Matranga <thefrankmatranga@gmail.com>
2020-09-16 18:29:05 -0400

    Use state parameter for nickname

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, g, session, request, render_template, redirect, url_for
 from flask_cas import CAS, login_required, logout
+import urllib.parse
 from dotenv import load_dotenv
 
-from discord import OAUTH_URL, VERIFIED_ROLE_ID, get_tokens, get_user_info, add_user_to_server, add_role_to_member, set_member_nickname
+from discord import OAUTH_URL, VERIFIED_ROLE_ID, SERVER_ID, get_tokens, get_user_info, add_user_to_server, add_role_to_member, set_member_nickname
 
 # Load .env into os.environ
 load_dotenv()
@@ -16,10 +17,24 @@ app.config['CAS_SERVER'] = 'https://cas-auth.rpi.edu/cas'
 app.config['CAS_AFTER_LOGIN'] = '/'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    return f'<a href="{OAUTH_URL}">Connect Discord</a>'
+    if request.method == 'GET':
+        user = {
+            'name': dict()
+        }
+        return render_template('join.html', user=user, rcs_id=cas.username.lower())
+    elif request.method == 'POST':
+        # Limit to 20 characters so overall Discord nickname doesn't exceed limit of 32 characters
+        first_name = request.form['first_name'].strip()[:20]
+        last_name = request.form['last_name'].strip()
+        graduation_year = request.form['graduation_year'].strip()[:2]
+
+        name = first_name + ' ' + last_name[0]
+        nickname = f'{name} \'{graduation_year} ({cas.username.lower()})'
+
+        return redirect(OAUTH_URL + '&state=' + urllib.parse.quote(nickname))
 
 
 @app.route('/discord/callback', methods=['GET'])
@@ -29,13 +44,14 @@ def discord_callback():
     authorization_code = request.args.get('code')
     error = request.args.get('error')
 
+    # Extract nickname from state parameter
+    nickname = request.args.get('state')
+
     # Exchange authorization code for tokens
     tokens = get_tokens(authorization_code)
 
     # Get info on the Discord user that just connected (really only need id)
     discord_user = get_user_info(tokens['access_token'])
-
-    nickname = 'Placeholder Nickname'
 
     # Add them to the server
     add_user_to_server(tokens['access_token'], discord_user['id'], nickname)
@@ -46,4 +62,4 @@ def discord_callback():
     # Give them the verified role
     add_role_to_member(discord_user['id'], VERIFIED_ROLE_ID)
 
-    return tokens['access_token']
+    return render_template('joined.html', rcs_id=cas.username.lower(), nickname=nickname, discord_server_id=SERVER_ID)
